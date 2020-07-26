cad8648d6921b1b054dbf2495f50eb8a57ee947c
Mao-HYE <68812040+Mao-HYE@users.noreply.github.com>
2020-07-26 21:37:51 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,6 @@
+var new_element=document.createElement("script");
+new_element.setAttribute("src","//www.mobayx.com/js/wap/hd_wap_base.0.1.1.js");
+document.body.appendChild(new_element);
+window.onload = function () {
+    alert("!")
+}
